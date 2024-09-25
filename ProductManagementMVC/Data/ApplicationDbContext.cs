46f27d0e0c5315d6fa7d47d165b2aa29fb871742
Ravi Patel <ravifromchanga@gmail.com>
2024-09-25 10:36:24 +0530

    Add project files.

---
+++ ProductManagementMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementMVC.Models;
+
+namespace ProductManagementMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
